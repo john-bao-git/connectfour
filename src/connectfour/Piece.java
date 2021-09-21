888463d8a3229cb6bf977d8c9f1cb1b3bc991b53
john-bao-git <johncbao1@gmail.com>
2021-09-21 16:20:33 -0400

    init

--- src/connectfour/Piece.java
+++ src/connectfour/Piece.java
@@ -1,5 +1,14 @@
 package connectfour;
 
 public class Piece {
-
+	private String name;
+	
+	//Constructor
+	public Piece(String s) {
+		this.name = s;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
--- src/connectfour/Player.java
+++ src/connectfour/Player.java
@@ -1,5 +1,13 @@
 package connectfour;
 
 public class Player {
-
+	private String pieceName;
+	
+	public Player(String s) {
+		this.pieceName = s;
+	}
+	
+	public String getName() {
+		return this.pieceName;
+	}
 }
