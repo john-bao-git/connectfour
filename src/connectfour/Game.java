77f8d28acda94528bb3b83497104d56d5b304937
john-bao-git <johncbao1@gmail.com>
2021-09-21 16:22:05 -0400

    init Game

--- src/connectfour/Game.java
+++ src/connectfour/Game.java
@@ -1,10 +1,23 @@
 package connectfour;
 
+import java.util.Scanner;
+
 public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Board board = new Board();
+		Scanner sc = new Scanner(System.in);
+		
+		board.init();
+		
+		//Init Players
+		Player pOne = new Player("O");
+		Player pTwo = new Player("X");
+		
+		//Do win-con
+		//Do turn
+		//Make a while loop for the game
 	}
-
 }
